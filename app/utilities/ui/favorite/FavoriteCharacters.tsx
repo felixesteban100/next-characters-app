2bb00be0df4f4e86bc12cce8825c0131f04135eb
felixesteban100 <felixesteban100@gmail.com>
2024-02-08 12:05:45 -0500

    trying to not get maximun call error 6

--- app/utilities/ui/favorite/FavoriteCharacters.tsx
+++ app/utilities/ui/favorite/FavoriteCharacters.tsx
@@ -1,43 +1,43 @@
-'use client';
+// 'use client';
 
-import CharacterComponent from '../characters/CharacterComponent';
-import CharactersContainer from '../characters/CharactersContainer';
-// import NoCharactersFound from '../characters/NoCharactersFound';
-import useCharacterStorage from '../../hooks/useCharacterStorage';
+// import CharacterComponent from '../characters/CharacterComponent';
+// import CharactersContainer from '../characters/CharactersContainer';
+// // import NoCharactersFound from '../characters/NoCharactersFound';
+// import useCharacterStorage from '../../hooks/useCharacterStorage';
 
-type FavoriteCharactersProps = {};
+// type FavoriteCharactersProps = {};
 
-export default function FavoriteCharacters({ }: FavoriteCharactersProps) {
-    const [charactersFavorite] = useCharacterStorage();
+// export default function FavoriteCharacters({ }: FavoriteCharactersProps) {
+//     const [charactersFavorite] = useCharacterStorage();
 
-    return (
-        <div>
-            <div className='w-full flex justify-between items-center gap-5 my-10 px-8'>
-                <h2 className="text-3xl font-bold">Favorites Characters</h2>
-                <p>Characters No.: {charactersFavorite.length}</p>
-            </div>
-            <div className="px-8 flex flex-col gap-10 bg-transparent">
-                {/* charactersFavorite.length > 0 ? ( */
-                    <CharactersContainer>
-                        <>
-                            {charactersFavorite.map((currentCharacter, index) => (
-                                <CharacterComponent
-                                    key={currentCharacter.slug}
-                                    index={index}
-                                    currentCharacter={JSON.parse(
-                                        JSON.stringify({ ...currentCharacter, _id: currentCharacter._id.toString() })
-                                    )}
-                                    withPagination={true}
-                                />
-                            ))}
-                        </>
-                    </CharactersContainer>
-                    /*  ) : (
-                         <NoCharactersFound />
-                     ) */
-                }
-            </div>
-        </div>
+//     return (
+//         <div>
+//             <div className='w-full flex justify-between items-center gap-5 my-10 px-8'>
+//                 <h2 className="text-3xl font-bold">Favorites Characters</h2>
+//                 <p>Characters No.: {charactersFavorite.length}</p>
+//             </div>
+//             <div className="px-8 flex flex-col gap-10 bg-transparent">
+//                 {/* charactersFavorite.length > 0 ? ( */
+//                     <CharactersContainer>
+//                         <>
+//                             {charactersFavorite.map((currentCharacter, index) => (
+//                                 <CharacterComponent
+//                                     key={currentCharacter.slug}
+//                                     index={index}
+//                                     currentCharacter={JSON.parse(
+//                                         JSON.stringify({ ...currentCharacter, _id: currentCharacter._id.toString() })
+//                                     )}
+//                                     withPagination={true}
+//                                 />
+//                             ))}
+//                         </>
+//                     </CharactersContainer>
+//                     /*  ) : (
+//                          <NoCharactersFound />
+//                      ) */
+//                 }
+//             </div>
+//         </div>
 
-    );
-}
+//     );
+// }
